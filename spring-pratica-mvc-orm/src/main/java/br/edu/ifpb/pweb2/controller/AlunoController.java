bb7661c9d70ab8bdb11cba8d7a42a8fd61611bc8
Felipe Rodrigues <30394677+felipersdf@users.noreply.github.com>
2019-10-03 17:03:34 -0300

    adding pratica 7 de spring

---
+++ spring-pratica-mvc-orm/src/main/java/br/edu/ifpb/pweb2/controller/AlunoController.java
@@ -0,0 +1,51 @@
+package br.edu.ifpb.pweb2.controller;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifpb.pweb2.dao.AlunoDAO;
+import br.edu.ifpb.pweb2.model.Aluno;
+
+@Controller
+@RequestMapping("/alunos")
+public class AlunoController {
+	@Autowired
+	AlunoDAO dao;
+	
+	@RequestMapping("/cadastreAluno")
+	public ModelAndView cadastreAluno(Aluno aluno) {
+		dao.gravar(aluno);
+		return listeAlunos();
+	}
+	
+	@RequestMapping("/showAlunoForm")
+	public String showAlunoForm(Model model) {
+		model.addAttribute("aluno", new Aluno());
+		model.addAttribute("cursoOptions", this.getCursoOptions());
+		return "form";
+	}
+	
+	@RequestMapping("/listeAlunos")
+	public ModelAndView listeAlunos() {
+		ModelAndView mav = new ModelAndView("lista");
+		List<Aluno> alunos = dao.findAll();
+		mav.addObjects("alunos", alunos);
+		return mav;
+	}
+	
+	
+	private Map<String, String> getCursoOptions() {
+		LinkedHashMap<String, String> options = new LinkedHashMap<String, String>();
+		options.put("TSI", "TSI");
+		options.put("REDES", "REDES");
+		options.put("ENG. ELÉTRICA", "ENG. ELÉTRICA");
+		return options;
+	}
+}
---
+++ spring-pratica-mvc-orm/src/main/java/br/edu/ifpb/pweb2/model/Aluno.java
@@ -0,0 +1,38 @@
+package br.edu.ifpb.pweb2.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_aluno")
+public class Aluno {
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String nome;
+	private Long matricula;
+	private String curso;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Long getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(Long matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	
+}
