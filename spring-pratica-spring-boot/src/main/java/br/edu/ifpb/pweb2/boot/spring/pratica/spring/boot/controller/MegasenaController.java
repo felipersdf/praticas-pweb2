590d7e87e3ce29d37bc660a90f99d5c5ab211798
Felipe Rodrigues <30394677+felipersdf@users.noreply.github.com>
2019-10-14 17:11:24 -0300

    adding pratica-spring-boot

---
+++ spring-pratica-spring-boot/src/main/java/br/edu/ifpb/pweb2/boot/spring/pratica/spring/boot/controller/MegasenaController.java
@@ -0,0 +1,25 @@
+package br.edu.ifpb.pweb2.boot.spring.pratica.spring.boot.controller;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MegasenaController {
+
+	private Set<Integer> aposta = null;
+	
+	@RequestMapping("/aposta")
+	public String gereNumeros(Model model) {
+		aposta = new LinkedHashSet<Integer>(6);
+		while(aposta.size() < 6) {
+			aposta.add(ThreadLocalRandom.current().nextInt(1,60));
+		}
+		model.addAttribute("aposta", aposta);
+		return "aposta";
+	}
+}
